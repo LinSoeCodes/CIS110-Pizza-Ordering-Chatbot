40d9ee51bbcc7faa98cd94fb61ea0e9c6d99993a
username <useremail>
2024-04-20 09:30:35 -0700

    Testing

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,1 +1,2 @@
-print("Hello World")
+print("Hi there! I am Soe, your virtual assistant. I can help you order a pizza!")
+print("I am going to ask what you want to order. After typing an answer, press Enter.")
