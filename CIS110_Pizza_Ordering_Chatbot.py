c9c0a67bc80f4cef41079469bc7cadbee4cc0454
username <useremail>
2024-04-20 10:16:44 -0700

    Assigned 5 variables

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,2 +1,16 @@
 print("Hi there! I am Soe, your virtual assistant. I can help you order a pizza!")
 print("I am going to ask what you want to order. After typing an answer, press Enter.")
+username = input("\nEnter you name:  ")
+print(f"\nHello, {username}. Nice to meet you!")
+size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+flavor = input("\nEnter the flavor of pizza:  ")
+print("If you are not sure what type of crust to get, I suggest if you love a thick, doughy crust choose the Handmade Pan. For thinner crust, select the Hand Tossed pizza. For the crispiest bite, choose the Crunch thin Crust.")
+crustType = input("\nWhat type of crust do you want:  ")
+quantity = input("\nHow many of these do you want to order? Enter a numberic value:  ")
+quantity = int(quantity)
+method = input("\nDo you want to do carry out or delivery?:  ")
+
+salesTax = 1.1 
+pizzaCost = 14.99
+total = (pizzaCost * quantity) * salesTax
+print(total) 
