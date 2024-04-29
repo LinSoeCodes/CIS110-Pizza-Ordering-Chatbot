b95c95a27a9ddac4602e42b70dec920de2785ef8
username <useremail>
2024-04-28 20:06:55 -0700

    If Else

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,7 +1,11 @@
 print("Hi there! I am Soe, your virtual assistant. I can help you order a pizza!")
 print("I am going to ask what you want to order. After typing an answer, press Enter.")
-username = input("\nEnter you name:  ")
-print(f"\nHello, {username}. Nice to meet you!")
+userName = input("\nEnter you name:  ")
+if userName.lower() == "Soe Lin":
+    print(f"\nMy usual customer, {userName} has returned. Welcome back, How many more pizzas do you want to order again?")
+else:
+    print(f"\nHello, {userName}. Nice to meet you! It's my pleasure to assist you ordering a pizza")
+    
 size = input("\nWhat size do you want? Enter small, medium, or large:  ")
 flavor = input("\nEnter the flavor of pizza:  ")
 print("If you are not sure what type of crust to get, I suggest if you love a thick, doughy crust choose the Handmade Pan. For thinner crust, select the Hand Tossed pizza. For the crispiest bite, choose the Crunch thin.")
@@ -10,9 +14,16 @@ quantity = input("\nHow many of these do you want to order? Enter a numberic val
 quantity = int(quantity)
 method = input("\nDo you want to do carry out or delivery?:  ")
 salesTax = 1.1 
-pizzaCost = 14.99
+
+if size.lower() == "small":
+    pizzaCost = 8.00
+elif size.lower() == "medium":
+    pizzaCost = 14.99
+elif size.lower() == "large": 
+    pizzaCost = 17.99 
+
 total = (pizzaCost * quantity) * salesTax
 print("-" * 10)
-print(f"Thank you, {username}, for your order.") 
+print(f"Thank you, {userName}, for your order.") 
 print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
 print("-" * 10)
