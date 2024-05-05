7a37a4dcf3df0e850bfa7666e9cec6ebcbbe2442
username <useremail>
2024-05-04 22:36:53 -0700

    So far so good

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,18 +1,25 @@
 print("Hi there! I am Soe, your virtual assistant. I can help you order a pizza!")
 print("I am going to ask what you want to order. After typing an answer, press Enter.")
-userName = input("\nEnter you name:  ")
-
-if userName.lower() == "Soe Lin":
+userName = input("\nEnter you name: ")
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name: ")
+if userName.lower() == "soe lin":
     print(f"\nMy usual customer, {userName} has returned. Welcome back, How many more pizzas do you want to order again?")
 else:
     print(f"\nHello, {userName}. Nice to meet you! It's my pleasure to assist you ordering a pizza")   
-size = input("\nWhat size do you want? Enter small, medium, or large:  ")
-flavor = input("\nEnter the flavor of pizza:  ")
+size = input("\nWhat size do you want? Enter small, medium, or large: ")
+while len(size) == 0:
+    size = input ("Invalid size! Please enter small, medium, or large: ")
+flavor = input("\nEnter the flavor of pizza: ")
+while len(flavor) == 0:
+    flavor = input ("Where is the flavor? You want a cardboard? To get to flavor town, you need to type in a valid flavor: ")
 print("If you are not sure what type of crust to get, I suggest if you love a thick, doughy crust choose the Handmade Pan. For thinner crust, select the Hand Tossed pizza. For the crispiest bite, choose the Crunch thin.")
-crustType = input("\nWhat type of crust do you want:  ")
-quantity = input("\nHow many of these do you want to order? Enter a numberic value:  ")
+crustType = input("\nWhat type of crust do you want: ")
+while len(crustType) == 0:
+    crustType = input("If there is no crust, it's like eating a sandwich. Please enter a valid pizza crust: ")
+quantity = input("\nHow many of these do you want to order? Enter a numberic value: ")
 quantity = int(quantity)
-method = input("\nDo you want to do carry out or delivery?:  ")
+method = input("\nDo you want to do carry out or delivery?: ")
 
 if method.lower() == "delivery":
     deliveryFee = 5 
@@ -29,10 +36,10 @@ elif size.lower() == "large":
 total = (pizzaCost * quantity) * salesTax + deliveryFee 
 print("-" * 10)
 print(f"Thank you, {userName}, for your order.") 
-print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
+print(f"Including tax, Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
 
 if total >= 50: 
     print("\nCongratulations! You have been awarded a $10 Off coupon for your next order.")
 else:
-    print("\nOrers over $50 will recive a free $10 Off Coupon for the next purchase.") 
+    print("\nOrders over $50 will recive a free $10 Off Coupon for the next purchase.") 
 print("-" * 10)
